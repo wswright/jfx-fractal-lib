976620783db4121ce5f85fbfa736536bbfa4b0c1
William Scott Wright <CPress.Scott@gmail.com>
2022-02-18 03:36:29 -0500

    Initial commit of jfx-fractal-lib after modularizing jfx-fractal.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module org.example.fractal.lib {
+	requires org.jetbrains.annotations;
+	exports org.example.fractal.lib;
+}
---
+++ src/main/java/org/example/fractal/lib/IFractalEquation.java
@@ -0,0 +1,15 @@
+package org.example.fractal.lib;
+
+import org.jetbrains.annotations.NotNull;
+
+public interface IFractalEquation {
+	/**
+	 * Calculate the number of iterations a point takes to leave a bound.
+	 * @param z Real part
+	 * @param c Complex part
+	 * @return Returns a ComplexAlgebraicForm
+	 */
+	@NotNull
+	ComplexAlgebraicForm calculateFractalIteration(ComplexAlgebraicForm z, ComplexAlgebraicForm c);
+	String getDisplayName();
+}
